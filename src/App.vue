ac181b549f3912765bc168e8a6e233238076240c
youlinaixu2017 <93694229+youlinaixu2017@users.noreply.github.com>
2022-07-03 20:34:45 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+
+
+import MouseEffects from './components/MouseEffects.vue';
+
+import Eletnav from './components/eletnav.vue';
+
+</script>
+
+<template>
+  <Eletnav></Eletnav>
+  
+
+
+  
+  <router-view></router-view>
+  
+
+  <MouseEffects></MouseEffects>
+
+  <rout-link></rout-link>
+</template>
+
+<style>
+
+</style>
